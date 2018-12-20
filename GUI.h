8ce5c2a42b94e9ace1275b8bdf50681c7bcb272f
NicolasAudepin <nicolas.audepin@gmail.com>
2018-12-20 02:32:23 +0100

    y a un pb avec les destroys

--- GUI.h
+++ GUI.h
@@ -1,13 +1,17 @@
 #ifndef GUI_H
 #define GUI_H
 
-
+#include "Weapon.h"
 #include "CrewMember.h"
+#include <string>
 
 class GUI{
     public:
     GUI();
+    void showWeapon(Weapon & w);
     void showCrewMember(CrewMember & cm);
+
+    std::string setlength(std::string txt, int len);
 };
 
 #endif
--- main.cc
+++ main.cc
@@ -1,14 +1,32 @@
 #include <string>
+#include <iostream>
 #include "GUI.h"
 #include "CrewMember.h"
+#include "Weapon.h"
 
 using namespace std;
 
 int main(){
     GUI gui = GUI();
-    CrewMember cm =  CrewMember("Joe3000");
+
+   
+
+
+    CrewMember cm =  CrewMember("CyberJohnatan");
     gui.showCrewMember( cm);
 
     cm.~CrewMember();
+
+    Weapon cb = Weapon("CyberGun300", 10);
+
+    Weapon bw = Weapon("GoodOldEfficientGun",50);
+
+    gui.showWeapon(cb);
+    gui.showWeapon(bw);
+    
+    
+    cb.~Weapon();
+
+    bw.~Weapon();
     return 0;
 }
